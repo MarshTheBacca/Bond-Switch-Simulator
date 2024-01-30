9e28709689cb6fa7b7f68015c8cf43d1d8b54463
MarshTheBacca <marshallhunt@live.com>
2024-01-30 19:38:32 +0000

    removed 'using namespace std;'

--- include/network.h
+++ include/network.h
@@ -12,7 +12,6 @@
 
 #include <spdlog/spdlog.h>
 
-using namespace std;
 using LoggerPtr = std::shared_ptr<spdlog::logger>;
 
 class Network
@@ -33,7 +32,7 @@ public:
     VecF<double> pb, rpb; //(reciprocal) periodic boundary
     int maxNetCnxs, maxDualCnxs;
     VecR<Node> nodes;                 // list of nodes
-    string geometryCode;              // geometry of system
+    std::string geometryCode;         // geometry of system
     VecF<int> nodeDistribution;       // number of each type of node
     VecF<VecF<int>> edgeDistribution; // number of each type of edge
     double SiScaling = 1.609 * sqrt((32.0 / 9.0)) / 0.52917721090380;
@@ -41,15 +40,15 @@ public:
     // Constructors
     Network();
     Network(int nNodes, int maxCnxs);
-    Network(int nNodes, string lattice, int maxCnxs, LoggerPtr logger);          // construct with default lattice
-    Network(string prefix, int maxNetCnxsA, int maxDualCnxsA, LoggerPtr logger); // construct by loading from files
-    Network(VecR<Node> nodesA, VecF<double> pbA, VecF<double> rpbA, string type, int maxNetCnxsA, int maxDualCnxsA);
+    Network(int nNodes, std::string lattice, int maxCnxs, LoggerPtr logger);          // construct with default lattice
+    Network(std::string prefix, int maxNetCnxsA, int maxDualCnxsA, LoggerPtr logger); // construct by loading from files
+    Network(VecR<Node> nodesA, VecF<double> pbA, VecF<double> rpbA, std::string type, int maxNetCnxsA, int maxDualCnxsA);
 
     // Member Functions
     Network constructDual(int maxCnxs, LoggerPtr logger);          // make dual graph
     void generateAuxConnections(Network dualNetwork, int auxType); // generate auxilary connections
     void rescale(double scaleFactor);                              // rescale coordinates
-    void project(string projType, double param);                   // project lattice onto different geometry
+    void project(std::string projType, double param);              // project lattice onto different geometry
     void findLocalRegion(int a, int b, int extent, VecR<int> &local, VecR<int> &fixedInner, VecR<int> &fixedOuter);
     VecF<double> getNodeDistribution();                                                // proportion of each type of node
     VecF<VecF<double>> getEdgeDistribution();                                          // proportion of each type of node
@@ -59,11 +58,11 @@ public:
     VecF<double> entropy();                                                            // calculate entropy of node and edge distribution
     VecF<int> maxClusters(int minCnd, int maxCnd, int minInnerCnxs, int minOuterCnxs); // get cluster statistics for node coordinations
     double maxCluster(int nodeCnd);                                                    // get cluster statistics for given node coordination
-    void write(string prefix);
-    void writeXYZ(string prefix, string element);
-    void writeBilayerA(string prefix, float cutoff);
-    void writeBilayerB(string prefix);
-    void writeBN(string prefix);
+    void write(std::string prefix);
+    void writeXYZ(std::string prefix, std::string element);
+    void writeBilayerA(std::string prefix, float cutoff);
+    void writeBilayerB(std::string prefix);
+    void writeBN(std::string prefix);
     bool r_ij(int i, int j, float cutoff);
 };
 
