77c8fdb8229b324ebfa8e6d6b7e2f461a52384d9
MarshTheBacca <marshallhunt@live.com>
2024-02-16 16:13:36 +0000

    moved all initialisation into one function for network.  Stripped down functionality for multiple types of network in one run.

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,11 @@ int main(int argc, char *argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
 
     // Create a file sink and a console sink with different names for clarity
-    auto file_sink =
-        std::make_shared<spdlog::sinks::basic_file_sink_mt>("./netmc.log", true);
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("./netmc.log", true);
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
     // Combine the sinks into a multi-sink logger
-    auto logger = std::make_shared<spdlog::logger>(
-        "multi_sink", spdlog::sinks_init_list{file_sink, console_sink});
+    auto logger = std::make_shared<spdlog::logger>("multi_sink", spdlog::sinks_init_list{file_sink, console_sink});
     spdlog::register_logger(logger);
     logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
 
@@ -42,7 +40,7 @@ int main(int argc, char *argv[]) {
     }
 
     logger->info("Network Monte Carlo");
-    logger->info("Written by David Ormrod Morely, Edited by Oliver Whitaker and "
+    logger->info("Written by David Ormrod Morley, Edited by Oliver Whitaker and "
                  "Marshall Hunt, Wilson Group, 2024");
 
     // Read input file
@@ -82,39 +80,8 @@ int main(int argc, char *argv[]) {
         network = LinkedNetwork(inputData, logger);
     }
 
-    int maxRingSize = network.maxBCnxs;
-
-    // Find Fixed Rings
-    network.pushPrefix(inputData.inputFolder, inputData.outputFolder);
-    network.findFixedRings(inputData.isFixRingsEnabled,
-                           inputData.inputFolder + "/" + inputData.fixedRingsFile,
-                           logger);
-
-    logger->info("Initialising potential model...");
-    network.initialisePotentialModel(inputData.harmonicAngleForceConstant, inputData.harmonicBondForceConstant,
-                                     inputData.harmonicGeometryConstraint,
-                                     inputData.isMaintainConvexityEnabled, logger);
-
-    logger->info("Initialising Geometry Optimisation...");
-    network.initialiseGeometryOpt(inputData.monteCarloLocalMaxIterations,
-                                  inputData.tauBacktrackingParameter,
-                                  inputData.tolerance, inputData.localRegionSize);
-
-    logger->info("Initialising Monte Carlo...");
-    network.initialiseMonteCarlo(network.networkA,
-                                 pow(10, inputData.startTemperature), logger,
-                                 inputData.randomSeed);
-
-    network.isOpenMPIEnabled = inputData.isOpenMPIEnabled;
-    network.mcWeighting = inputData.randomOrWeighted;
-    network.isSimpleGrapheneEnabled = inputData.isSimpleGrapheneEnabled;
-    network.isTersoffGrapheneEnabled = inputData.isTersoffGrapheneEnabled;
-    network.isTriangleRaftEnabled = inputData.isTriangleRaftEnabled;
-    network.isBilayerEnabled = inputData.isBilayerEnabled;
-    network.isBNEnabled = inputData.isBNEnabled;
-    network.mcRoutine = inputData.selectedMinimisationProtocol;
-
     logger->info("Network initialised!");
+    logger->info("Initial energy: {}", network.energy);
 
     // Initialise output files
     logger->info("Initialising analysis output files...");
@@ -147,53 +114,15 @@ int main(int argc, char *argv[]) {
     angHist = 0.0;
 
     // Initialise Monte Carlo variables
-    int numAcceptedMoves = 0;
-    int optIterations = 0;
-    VecF<int> optCodes(5);
-    optCodes = 0;
     VecF<int> moveStatus;
 
     // Run monte carlo thermalisation
     logger->info("Running Monte Carlo thermalisation...");
-    double energy = network.mc.getEnergy();
-    logger->info("Initial energy: {}", energy);
-    double SimpleGrapheneEnergy = 0.0;
-    double TersoffGrapheneEnergy = 0.0;
-    double TriangleRaftEnergy = 0.0;
-    double BilayerEnergy = 0.0;
-    double BNEnergy = 0.0;
     try {
-        if (inputData.isSimpleGrapheneEnabled) {
-            SimpleGrapheneEnergy = network.SimpleGraphene.globalPotentialEnergy();
-        }
-        if (inputData.isTersoffGrapheneEnabled) {
-            TersoffGrapheneEnergy = network.TersoffGraphene.globalPotentialEnergy();
-        }
-        if (inputData.isTriangleRaftEnabled) {
-            TriangleRaftEnergy = network.Triangle_Raft.globalPotentialEnergy();
-        }
-        if (inputData.isBilayerEnabled) {
-            BilayerEnergy = network.Bilayer.globalPotentialEnergy();
-        }
-        if (inputData.isBNEnabled) {
-            BNEnergy = network.BN.globalPotentialEnergy();
-        }
-
-        logger->info("Calculated energies for running systems");
         double expTemperature = pow(10, inputData.thermalisationTemperature);
         network.mc.setTemperature(expTemperature);
-
         for (int i = 1; i <= inputData.initialThermalisationSteps; ++i) {
-            logger->info("Equilibriating: {}  -- Energies: {} {} {} {} {}", i,
-                         SimpleGrapheneEnergy, TersoffGrapheneEnergy,
-                         TriangleRaftEnergy, BilayerEnergy, BNEnergy);
-            moveStatus = network.monteCarloSwitchMoveLAMMPS(SimpleGrapheneEnergy, TersoffGrapheneEnergy, TriangleRaftEnergy,
-                                                            BilayerEnergy, BNEnergy, logger);
-            logger->debug("After move");
-            if (moveStatus[0])
-                numAcceptedMoves++;
-            optCodes[moveStatus[1]] += 1;
-            optIterations += moveStatus[2];
+            network.monteCarloSwitchMoveLAMMPS(logger);
             if (i % inputData.analysisWriteFrequency == 0) {
                 VecF<double> ringStats = network.getNodeDistribution("B");
                 double r = network.getAssortativity("B");
@@ -201,8 +130,8 @@ int main(int argc, char *argv[]) {
                 VecF<double> aw = network.getAboavWeaire("B");
                 VecF<double> s = network.getEntropy("B");
                 VecF<double> corr(6);
-                VecF<double> a(maxRingSize + 1);
-                VecF<double> aSq(maxRingSize + 1);
+                VecF<double> a(inputData.maxRingSize + 1);
+                VecF<double> aSq(inputData.maxRingSize + 1);
                 double rr = network.getAssortativity("A");
                 corr[0] = r;
                 corr[1] = aEst;
@@ -210,30 +139,20 @@ int main(int argc, char *argv[]) {
                 corr[3] = aw[1];
                 corr[4] = aw[2];
                 corr[5] = rr;
-                VecF<double> eCompare(6);
-                eCompare[0] = ringStats[6];
-                eCompare[1] = SimpleGrapheneEnergy;
-                eCompare[2] = TersoffGrapheneEnergy;
-                eCompare[3] = TriangleRaftEnergy;
-                eCompare[4] = BilayerEnergy;
-                eCompare[5] = BNEnergy;
 
                 VecF<double> emptyL;
                 VecF<double> emptyA; // dummy histograms
-                VecF<double> geomStats =
-                    network.getOptimisationGeometry(network.networkA, emptyL, emptyA);
+                VecF<double> geomStats = network.getOptimisationGeometry(network.networkA, emptyL, emptyA);
                 VecF<VecF<int>> edgeDist = network.getEdgeDistribution("B");
                 VecF<double> cndStats = network.getNodeDistribution("A");
                 outRingStats.writeRowVector(ringStats);
                 outCorr.writeRowVector(corr);
-                outEnergy.write(energy);
+                outEnergy.write(network.energy);
                 outEntropy.writeRowVector(s);
                 outTemperature.write(expTemperature);
                 outGeometry.writeRowVector(geomStats);
                 outAreas.writeRowVector(a);
                 outAreas.writeRowVector(aSq);
-
-                outEnergyStats.writeRowVector(eCompare);
 
                 for (int j = 0; j < edgeDist.n; ++j)
                     outEmatrix.writeRowVector(edgeDist[j]);
@@ -241,7 +160,6 @@ int main(int argc, char *argv[]) {
             }
             if (i % inputData.structureWriteFrequency == 0 &&
                 inputData.isWriteSamplingStructuresEnabled == 1) {
-                network.syncCoordinates();
                 std::string structureFilePath = prefixOut + "_therm_" + std::to_string(i);
                 logger->info("Writing structure: {} to file {}", i, structureFilePath);
                 network.writeXYZ(structureFilePath);
@@ -254,20 +172,11 @@ int main(int argc, char *argv[]) {
 
         int numTemperatureSteps = std::floor((inputData.endTemperature - inputData.startTemperature) / inputData.temperatureIncrement);
         for (int temperature = 0; temperature <= numTemperatureSteps; ++temperature) {
-
             expTemperature = pow(10, inputData.startTemperature + temperature * inputData.temperatureIncrement);
             network.mc.setTemperature(expTemperature);
             logger->info("Temperature: {}", expTemperature);
             for (int i = 1; i <= inputData.stepsPerTemperature; ++i) {
-                logger->info("Running number: {} Energies: {} {} {} {} {}", i,
-                             SimpleGrapheneEnergy, TersoffGrapheneEnergy,
-                             TriangleRaftEnergy, BilayerEnergy, BNEnergy);
-                moveStatus = network.monteCarloSwitchMoveLAMMPS(SimpleGrapheneEnergy, TersoffGrapheneEnergy, TriangleRaftEnergy,
-                                                                BilayerEnergy, BNEnergy, logger);
-                if (moveStatus[0])
-                    numAcceptedMoves++;
-                optCodes[moveStatus[1]] += 1;
-                optIterations += moveStatus[2];
+                network.monteCarloSwitchMoveLAMMPS(logger);
                 if (i % inputData.analysisWriteFrequency == 0) {
 
                     VecF<double> ringStats = network.getNodeDistribution("B");
@@ -276,8 +185,8 @@ int main(int argc, char *argv[]) {
                     VecF<double> aw = network.getAboavWeaire("B");
                     VecF<double> s = network.getEntropy("B");
                     VecF<double> corr(6);
-                    VecF<double> a(maxRingSize + 1);
-                    VecF<double> aSq(maxRingSize + 1);
+                    VecF<double> a(inputData.maxRingSize + 1);
+                    VecF<double> aSq(inputData.maxRingSize + 1);
                     double rr = network.getAssortativity("A");
                     corr[0] = r;
                     corr[1] = aEst;
@@ -288,30 +197,21 @@ int main(int argc, char *argv[]) {
                     VecF<double> geomStats = network.getOptimisationGeometry(network.networkA, lenHist, angHist);
                     VecF<VecF<int>> edgeDist = network.getEdgeDistribution("B");
                     VecF<double> cndStats = network.getNodeDistribution("A");
-                    VecF<double> eCompare(6);
-                    eCompare[0] = ringStats[6];
-                    eCompare[1] = SimpleGrapheneEnergy;
-                    eCompare[2] = TersoffGrapheneEnergy;
-                    eCompare[3] = TriangleRaftEnergy;
-                    eCompare[4] = BilayerEnergy;
-                    eCompare[5] = BNEnergy;
 
                     outRingStats.writeRowVector(ringStats);
                     outCorr.writeRowVector(corr);
-                    outEnergy.write(energy);
+                    outEnergy.write(network.energy);
                     outEntropy.writeRowVector(s);
                     outTemperature.write(expTemperature);
                     outGeometry.writeRowVector(geomStats);
                     outAreas.writeRowVector(a);
                     outAreas.writeRowVector(aSq);
-                    outEnergyStats.writeRowVector(eCompare);
                     for (int j = 0; j < edgeDist.n; ++j)
                         outEmatrix.writeRowVector(edgeDist[j]);
                     outCndStats.writeRowVector(cndStats);
                 }
                 if (i % inputData.structureWriteFrequency == 0 &&
                     inputData.isWriteSamplingStructuresEnabled == 1) {
-                    network.syncCoordinates();
                     network.writeXYZ(prefixOut + "_t" + std::to_string(temperature) + "_" + std::to_string(i));
                 }
             }
@@ -332,21 +232,9 @@ int main(int argc, char *argv[]) {
         outGeomHist.writeRowVector(hist);
     }
 
-    if (inputData.isSimpleGrapheneEnabled) {
-        logger->info("Writing Simple Graphene Results");
-        network.SimpleGraphene.write_data("Si");
-        network.SimpleGraphene.write_restart("Si");
-    }
-    if (inputData.isTriangleRaftEnabled) {
-        logger->info("Writing Triangle Raft Results");
-        network.Triangle_Raft.write_data("Si2O3");
-        network.Triangle_Raft.write_restart("Si2O3");
-    }
-    if (inputData.isBNEnabled) {
-        logger->info("Writing BN Results");
-        network.BN.write_data("BN");
-        network.BN.write_restart("BN");
-    }
+    logger->info("Writing LAMMPS Results");
+    network.lammpsNetwork.write_data("Si");
+    network.lammpsNetwork.write_restart("Si");
 
     // Check network
     logger->debug("Diagnosing simulation...");
@@ -355,23 +243,15 @@ int main(int argc, char *argv[]) {
     bool convex = network.checkConvexity();
     logger->debug("Network consistent: {}", consistent ? "true" : "false");
     logger->debug("Rings convex: {}", convex ? "true" : "false");
-    logger->debug("Monte Carlo acceptance: {}", (double)numAcceptedMoves / inputData.stepsPerTemperature);
-    logger->debug("Geometry optimisation codes: {}");
-    logger->debug("Converged: {}", optCodes[0]);
-    logger->debug("Converged (zero force): {}", optCodes[1]);
-    logger->debug("Unconverged: {}", optCodes[2]);
-    logger->debug("Failed (overlapping): {}", optCodes[3]);
-    logger->debug("Failed (initially non-convex): {}", optCodes[4]);
-    logger->debug("Geometry optimisation average iterations: {}", optIterations / vSum(optCodes));
 
     // Write files
     logger->info("Writing files...");
-    network.wrapCoordinates();
-    network.syncCoordinates();
     network.write(prefixOut);
     network.writeXYZ(prefixOut);
     logger->info(prefixOut);
     logger->debug("Files written");
+
+    logger->info("Monte Carlo acceptance: {}", (double)network.numAcceptedSwitches / network.numSwitches);
 
     // Log time taken
     auto end = std::chrono::high_resolution_clock::now();
