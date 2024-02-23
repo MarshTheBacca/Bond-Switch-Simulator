f09fbdc80552d5212dc557801309af0f8f6097c4
MarshTheBacca <marshallhunt@live.com>
2024-02-23 00:12:29 +0000

    removed all use of VecF, VecR. Use of const & and std::for_each and initialiser lists.

--- include/node.h
+++ include/node.h
@@ -3,38 +3,30 @@
 #ifndef NL_NODE_H
 #define NL_NODE_H
 
-#include "vec_func.h"
-#include "vecf.h"
-#include "vecr.h"
+#include "vector_tools.h"
 #include <iostream>
 
 // Node in network, aka vertex in graph
 class Node {
   public:
     // Data members
-    int id;             // unique id used by other nodes for connections
-    VecF<double> crd;   // coordinate
-    VecR<int> netCnxs;  // connections to nodes in network
-    VecR<int> dualCnxs; // connections to nodes in dual
-    VecR<int> auxCnxs;  // free form additional connections to be repurposed as needed
-    VecR<int> oxyCnxs;  // hack as cannot return arrays in c++11
+    int id;                    // unique id used by other nodes for connections
+    std::vector<double> crd;   // coordinate
+    std::vector<int> netCnxs;  // connections to nodes in network
+    std::vector<int> dualCnxs; // connections to nodes in dual
 
     // Constructors
     Node();
-    Node(int nodeId, int maxNetCnxs, int maxDualCnxs, int maxAuxCnxs);
-    inline double distanceFrom(VecF<double> &coordinate) {
+    Node(const int &nodeID, const int &maxNetCnxs, const int &maxDualCnxs);
+
+    inline double distanceFrom(const std::vector<double> &coordinate) {
         return sqrt(pow(crd[0] - coordinate[0], 2) + pow(crd[1] - coordinate[1], 2));
     }
 
-    // Destructor
-    ~Node() = default;
-
     // Member functions
-
     std::string toString();
 };
 
-std::ostream &operator<<(std::ostream &os, Node &node);
 std::ostream &operator<<(std::ostream &os, const Node &node);
 
 #endif // NL_NODE_H
--- src/node.cpp
+++ src/node.cpp
@@ -1,49 +1,28 @@
 #include "node.h"
 
 // Default constructor
-Node::Node() {
-    id = -1;
-    crd = VecF<double>(1);
-    netCnxs = VecR<int>(1);
-    dualCnxs = VecR<int>(1);
-    auxCnxs = VecR<int>(1);
+Node::Node() : id(-1), crd(1), netCnxs(1), dualCnxs(1) {
 }
 
 // Construct with maximum number of connections, initialise with 0
-Node::Node(int nodeId, int maxNetCnxs, int maxDualCnxs, int maxAuxCnxs) : id(nodeId), netCnxs(0, maxNetCnxs),
-                                                                          dualCnxs(0, maxDualCnxs), auxCnxs(0, maxAuxCnxs) {
-    oxyCnxs = VecR<int>(8);
-}
-
-std::ostream &operator<<(std::ostream &os, Node &node) {
-    // Replace this with your actual implementation
-    os << "Node " << node.id << "at " << node.crd[0] << ", " << node.crd[1] << " with neighbours: ";
-    for (int i = 0; i < node.netCnxs.n; ++i)
-        os << node.netCnxs[i] << " ";
-    os << " and ring neighbours: ";
-    for (int i = 0; i < node.dualCnxs.n; ++i)
-        os << node.dualCnxs[i] << " ";
-    return os;
+Node::Node(const int &nodeId, const int &maxNetCnxs, const int &maxDualCnxs) : id(nodeId), netCnxs(0, maxNetCnxs),
+                                                                               dualCnxs(0, maxDualCnxs) {
 }
 
 std::ostream &operator<<(std::ostream &os, const Node &node) {
     // Replace this with your actual implementation
     os << "Node " << node.id << "at " << node.crd[0] << ", " << node.crd[1] << " with neighbours: ";
-    for (int i = 0; i < node.netCnxs.n; ++i)
-        os << node.netCnxs[i] << " ";
+    std::for_each(node.netCnxs.begin(), node.netCnxs.end(), [&os](int i) { os << i << " "; });
     os << " and ring neighbours: ";
-    for (int i = 0; i < node.dualCnxs.n; ++i)
-        os << node.dualCnxs[i] << " ";
+    std::for_each(node.dualCnxs.begin(), node.dualCnxs.end(), [&os](int i) { os << i << " "; });
     return os;
 }
 
 std::string Node::toString() {
     std::string str = "Node " + std::to_string(id) + " at " + std::to_string(crd[0]) + ", " + std::to_string(crd[1]) +
                       " with neighbours: ";
-    for (int i = 0; i < netCnxs.n; ++i)
-        str += std::to_string(netCnxs[i]) + " ";
+    std::for_each(netCnxs.begin(), netCnxs.end(), [&str](int i) { str += std::to_string(i) + " "; });
     str += " and ring neighbours: ";
-    for (int i = 0; i < dualCnxs.n; ++i)
-        str += std::to_string(dualCnxs[i]) + " ";
+    std::for_each(dualCnxs.begin(), dualCnxs.end(), [&str](int i) { str += std::to_string(i) + " "; });
     return str;
 }
