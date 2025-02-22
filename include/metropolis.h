2a9a6a5bffe6a88b3baf4947593766de6e2b637c
MarshTheBacca <marshallhunt@live.com>
2025-02-22 22:39:09 +0000

    Move documentation to header file. Pass basic data types by value. Use initialiser list for constructor

--- include/metropolis.h
+++ include/metropolis.h
@@ -5,13 +5,33 @@
 #include <random>
 
 struct Metropolis {
-    std::mt19937 randomNumGen = std::mt19937(0);
-    std::uniform_real_distribution<double> randNumDist = std::uniform_real_distribution<double>(0.0, 1.0);
+  // Random number generator algorithm
+  std::mt19937 randomNumGen = std::mt19937(0);
+  // Random number distribution
+  std::uniform_real_distribution<double> randNumDist =
+      std::uniform_real_distribution<double>(0.0, 1.0);
 
-    Metropolis();
-    explicit Metropolis(const int &seed);
+  /**
+   * @brief Default constructor with a random number generator seed of 0
+   */
+  Metropolis() = default;
 
-    bool acceptanceCriterion(const double &finalEnergy, const double &initialEnergy, const double &temperature);
+  /**
+   * @brief Construct with a given seed
+   * @param seed seed for random number generator
+   */
+  explicit Metropolis(const int seed);
+
+  /**
+   * @brief Acceptance criterion for Metropolis-Hastings algorithm
+   * @param finalEnergy Final energy
+   * @param initialEnergy Initial energy
+   * @param temperature Temperature factor
+   * @return True if final energy less than initial, or with probability
+   * e^(-deltaE/T)
+   */
+  bool acceptanceCriterion(const double finalEnergy, const double initialEnergy,
+                           const double temperature);
 };
 
 #endif // METROPOLIS_H
--- src/metropolis.cpp
+++ src/metropolis.cpp
@@ -1,26 +1,11 @@
 #include "metropolis.h"
 
-/**
- * @brief Default constructor with a random number generator seed of 0
- */
-Metropolis::Metropolis() = default;
+Metropolis::Metropolis(const int seed) : randomNumGen(seed) {}
 
-/**
- * @brief Construct with a given seed
- * @param seed seed for random number generator
- */
-Metropolis::Metropolis(const int &seed) {
-    randomNumGen.seed(seed);
-}
-
-/**
- * @brief Acceptance criterion for Metropolis-Hastings algorithm
- * @param finalEnergy Final energy
- * @param initialEnergy Initial energy
- * @param temperature Temperature factor
- * @return True if final energy less than initial, or with probability e^(-deltaE/T)
- */
-bool Metropolis::acceptanceCriterion(const double &finalEnergy, const double &initialEnergy, const double &temperature) {
-    const double energyChange = finalEnergy - initialEnergy;
-    return energyChange < 0 || randNumDist(randomNumGen) < exp(-energyChange / temperature);
+bool Metropolis::acceptanceCriterion(const double finalEnergy,
+                                     const double initialEnergy,
+                                     const double temperature) {
+  const double energyChange = finalEnergy - initialEnergy;
+  return energyChange < 0 ||
+         randNumDist(randomNumGen) < exp(-energyChange / temperature);
 }
