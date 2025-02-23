6b3ec80adc0b3a581ebc698aab3e2dfb660d484a
MarshTheBacca <marshallhunt@live.com>
2025-02-23 22:37:19 +0000

    Move documentation to header file. Use std::format more. Add `numConnections` and `numDualConnections`

--- include/node.h
+++ include/node.h
@@ -1,33 +1,82 @@
 #ifndef NL_NODE_H
 #define NL_NODE_H
 
-#include "vector_tools.h"
-#include <iostream>
+#include <array>
+#include <cmath>
+#include <set>
+#include <string>
 
 struct Node {
-    // Member variables
-    int id = 0;                        // unique id used by other nodes for connections
-    std::vector<double> crd{0.0, 0.0}; // coordinate
-    std::vector<int> netConnections;          // connections to nodes in network
-    std::vector<int> dualConnections;         // connections to nodes in dual network
+  // Unique ID number
+  int id = 0;
+  // Location of the node in 2D euclidean space
+  std::array<double, 2> coord{0.0, 0.0};
+  // IDs of connected nodes in the same network
+  std::set<int> netConnections{};
+  // IDs of connected nodes in the dual network
+  std::set<int> dualConnections{};
 
-    // Constructors
-    Node();
-    explicit Node(const int &nodeID);
-    Node(const int &nodeID, const std::vector<double> &crd);
-    Node(const int &nodeID, const std::vector<double> &crd, const std::vector<int> &netConnections, const std::vector<int> &dualConnections);
+  /**
+   * @brief Construct a node with an ID of 0, no connections and coordinate at
+   * (0, 0)
+   */
+  Node() = default;
 
-    // Methods
-    std::string toString() const;
+  /**
+   * @brief Construct node with a given ID with no connections and coordinate at
+   * (0, 0)
+   * @param nodeId The ID of the node
+   */
+  explicit Node(const int nodeID);
 
-    /**
-     * @brief Calculate the distance between this node and a given coordinate
-     * @param coordinate The coordinate to calculate the distance to
-     * @return distance
-     */
-    inline double distanceFrom(const std::vector<double> &coordinate) {
-        return sqrt(pow(crd[0] - coordinate[0], 2) + pow(crd[1] - coordinate[1], 2));
-    }
+  /**
+   * @brief Constructor with a given node ID and coordinates and no connections
+   * @param nodeID The ID of the node
+   * @param coord The coordinates of the node
+   */
+  Node(const int nodeID, const std::array<double, 2> &coord);
+
+  /**
+   * @brief Constructor with a given node ID, coordinates and connections
+   * @param nodeID The ID of the node
+   * @param coord The coordinates of the node
+   * @param netConnections The connections to nodes in the network
+   * @param dualConnections The connections to nodes in the dual network
+   */
+  Node(const int nodeID, const std::array<double, 2> &coord,
+       const std::set<int> &netConnections,
+       const std::set<int> &dualConnections);
+
+  /**
+   * @brief Convert the node to a string
+   */
+  std::string toString() const;
+
+  /**
+   * @brief Calculate the distance between this node and a given coordinate
+   * @param coordinate The coordinate to calculate the distance to
+   * @return distance
+   */
+  inline double distanceFrom(const std::array<double, 2> &coordinate) {
+    return sqrt(pow(coord[0] - coordinate[0], 2) +
+                pow(coord[1] - coordinate[1], 2));
+  }
+
+  /**
+   * @brief Get the node's number of connections
+   * @return Number of connections
+   */
+  inline int numConnections() const {
+    return static_cast<int>(netConnections.size());
+  }
+
+  /**
+   * @brief Get the node's number of dual connections
+   * @return Number of dual connections
+   */
+  inline int numDualConnections() const {
+    return static_cast<int>(netConnections.size());
+  }
 };
 
 #endif // NL_NODE_H
--- src/node.cpp
+++ src/node.cpp
@@ -1,44 +1,30 @@
 #include "node.h"
+#include <algorithm>
+#include <array>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <string>
 
-/**
- * @brief Construct a node with an ID of 0, no connections and coordinate at [0, 0]
- */
-Node::Node() = default;
+Node::Node(const int nodeId) : id(nodeId) {}
 
-/**
- * @brief Construct node with a given ID with no connections and coordinate at [0, 0]
- * @param nodeId The ID of the node
- */
-Node::Node(const int &nodeId) : id(nodeId) {
-}
+Node::Node(const int nodeID, const std::array<double, 2> &coord)
+    : id(nodeID), coord(coord) {}
 
-/**
- * @brief Constructor with a given node ID and coordinates and no connections
- * @param nodeID The ID of the node
- * @param crd The coordinates of the node
- */
-Node::Node(const int &nodeID, const std::vector<double> &crd) : id(nodeID), crd(crd) {
-}
+Node::Node(const int nodeID, const std::array<double, 2> &coord,
+           const std::set<int> &netConnections,
+           const std::set<int> &dualConnections)
+    : id(nodeID), coord(coord), netConnections(netConnections),
+      dualConnections(dualConnections) {}
 
-/**
- * @brief Constructor with a given node ID, coordinates and connections
- * @param nodeID The ID of the node
- * @param crd The coordinates of the node
- * @param netConnections The connections to nodes in the network
- * @param dualConnections The connections to nodes in the dual network
- */
-Node::Node(const int &nodeID, const std::vector<double> &crd, const std::vector<int> &netConnections, const std::vector<int> &dualConnections)
-    : id(nodeID), crd(crd), netConnections(netConnections), dualConnections(dualConnections) {
-}
-
-/**
- * @brief Convert the node to a string
- */
 std::string Node::toString() const {
-    std::string str = "Node " + std::to_string(id) + " at " + std::to_string(crd[0]) + ", " + std::to_string(crd[1]) +
-                      " with neighbours: ";
-    std::for_each(netConnections.begin(), netConnections.end(), [&str](int i) { str += std::to_string(i) + " "; });
-    str += " and ring neighbours: ";
-    std::for_each(dualConnections.begin(), dualConnections.end(), [&str](int i) { str += std::to_string(i) + " "; });
-    return str;
+  std::ostringstream oss;
+  oss << std::format("Node {} at {}, {} with neighbours: ", id, coord[0],
+                     coord[1]);
+  std::ranges::for_each(netConnections,
+                        [&oss](int i) { oss << std::format("{} ", i); });
+  oss << " and ring neighbours: ";
+  std::ranges::for_each(dualConnections,
+                        [&oss](int i) { oss << std::format("{} ", i); });
+  return oss.str();
 }
