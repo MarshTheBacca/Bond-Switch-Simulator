176cf05c4010178eae80ec0e2424a1c179123a29
MarshTheBacca <marshallhunt@live.com>
2025-03-05 22:04:09 +0000

    Add `getRandomConnectionID`, use appropriate integer types

--- include/node.h
+++ include/node.h
@@ -3,18 +3,19 @@
 
 #include <array>
 #include <cmath>
+#include <cstdint>
 #include <set>
 #include <string>
 
 struct Node {
   // Unique ID number
-  int id = 0;
+  const uint16_t id = 0;
   // Location of the node in 2D euclidean space
   std::array<double, 2> coord{0.0, 0.0};
   // IDs of connected nodes in the same network
-  std::set<int> netConnections{};
+  std::set<uint16_t> netConnections{};
   // IDs of connected nodes in the dual network
-  std::set<int> dualConnections{};
+  std::set<uint16_t> dualConnections{};
 
   /**
    * @brief Construct a node with an ID of 0, no connections and coordinate at
@@ -22,34 +23,16 @@ struct Node {
    */
   Node() = default;
 
-  /**
-   * @brief Construct node with a given ID with no connections and coordinate at
-   * (0, 0)
-   * @param nodeId The ID of the node
-   */
-  explicit Node(const int nodeID);
+  explicit Node(const uint16_t nodeID);
 
-  /**
-   * @brief Constructor with a given node ID and coordinates and no connections
-   * @param nodeID The ID of the node
-   * @param coord The coordinates of the node
-   */
-  Node(const int nodeID, const std::array<double, 2> &coord);
+  Node(const uint16_t nodeID, const std::array<double, 2> &coord);
 
-  /**
-   * @brief Constructor with a given node ID, coordinates and connections
-   * @param nodeID The ID of the node
-   * @param coord The coordinates of the node
-   * @param netConnections The connections to nodes in the network
-   * @param dualConnections The connections to nodes in the dual network
-   */
-  Node(const int nodeID, const std::array<double, 2> &coord,
-       const std::set<int> &netConnections,
-       const std::set<int> &dualConnections);
+  Node(const uint16_t nodeID, const std::array<double, 2> &coord,
+       const std::set<uint16_t> &netConnections,
+       const std::set<uint16_t> &dualConnections);
 
-  /**
-   * @brief Convert the node to a string
-   */
+  Node &operator=(const Node &other);
+
   std::string toString() const;
 
   /**
@@ -57,7 +40,7 @@ struct Node {
    * @param coordinate The coordinate to calculate the distance to
    * @return distance
    */
-  inline double distanceFrom(const std::array<double, 2> &coordinate) {
+  inline double distanceFrom(const std::array<double, 2> &coordinate) const {
     return sqrt(pow(coord[0] - coordinate[0], 2) +
                 pow(coord[1] - coordinate[1], 2));
   }
@@ -66,17 +49,18 @@ struct Node {
    * @brief Get the node's number of connections
    * @return Number of connections
    */
-  inline int numConnections() const {
-    return static_cast<int>(netConnections.size());
-  }
+  inline size_t numConnections() const { return this->netConnections.size(); }
 
   /**
    * @brief Get the node's number of dual connections
    * @return Number of dual connections
    */
-  inline int numDualConnections() const {
-    return static_cast<int>(netConnections.size());
+  inline size_t numDualConnections() const {
+    return this->dualConnections.size();
   }
+
+  uint16_t getRandomNetConnectionID() const;
+  uint16_t getRandomDualConnectionID() const;
 };
 
 #endif // NL_NODE_H
--- src/node.cpp
+++ src/node.cpp
@@ -1,4 +1,5 @@
 #include "node.h"
+#include "random_number_generator.h"
 #include <algorithm>
 #include <array>
 #include <ranges>
@@ -6,17 +7,37 @@
 #include <sstream>
 #include <string>
 
-Node::Node(const int nodeId) : id(nodeId) {}
+/**
+ * @brief Construct node with a given ID with no connections and coordinate at
+ * (0, 0)
+ * @param nodeId The ID of the node
+ */
+Node::Node(const uint16_t nodeId) : id(nodeId) {}
 
-Node::Node(const int nodeID, const std::array<double, 2> &coord)
+/**
+ * @brief Constructor with a given node ID and coordinates and no connections
+ * @param nodeID The ID of the node
+ * @param coord The coordinates of the node
+ */
+Node::Node(const uint16_t nodeID, const std::array<double, 2> &coord)
     : id(nodeID), coord(coord) {}
 
-Node::Node(const int nodeID, const std::array<double, 2> &coord,
-           const std::set<int> &netConnections,
-           const std::set<int> &dualConnections)
+/**
+ * @brief Constructor with a given node ID, coordinates and connections
+ * @param nodeID The ID of the node
+ * @param coord The coordinates of the node
+ * @param netConnections The connections to nodes in the network
+ * @param dualConnections The connections to nodes in the dual network
+ */
+Node::Node(const uint16_t nodeID, const std::array<double, 2> &coord,
+           const std::set<uint16_t> &netConnections,
+           const std::set<uint16_t> &dualConnections)
     : id(nodeID), coord(coord), netConnections(netConnections),
       dualConnections(dualConnections) {}
 
+/**
+ * @brief Convert the node to a string
+ */
 std::string Node::toString() const {
   std::ostringstream oss;
   oss << std::format("Node {} at {}, {} with neighbours: ", id, coord[0],
@@ -27,4 +48,23 @@ std::string Node::toString() const {
   std::ranges::for_each(dualConnections,
                         [&oss](int i) { oss << std::format("{} ", i); });
   return oss.str();
+}
+
+uint16_t Node::getRandomNetConnectionID() const {
+  return *RandomNumberGenerator::getInstance().getRandomElement(
+      this->netConnections.begin(), this->netConnections.end());
+}
+
+uint16_t Node::getRandomDualConnectionID() const {
+  return *RandomNumberGenerator::getInstance().getRandomElement(
+      this->dualConnections.begin(), this->dualConnections.end());
+}
+
+Node &Node::operator=(const Node &other) {
+  if (this != &other) {
+    this->coord = other.coord;
+    this->netConnections = other.netConnections;
+    this->dualConnections = other.dualConnections;
+  }
+  return *this;
 }
