fa27219a89c245e80440392787d355fb63887dd3
MarshTheBacca <marshallhunt@live.com>
2025-07-10 21:45:42 +0100

    Remove `Metropolis` because only one member function with no member variables

--- include/utils.h
+++ include/utils.h
@@ -1,11 +1,15 @@
-#include "metropolis.h"
+#ifndef UTILS_H
+#define UTILS_H
+
 #include "random_number_generator.h"
 
-bool Metropolis::acceptanceCriterion(const double finalEnergy,
-                                     const double initialEnergy,
-                                     const double temperature) {
+constexpr bool acceptanceCriterion(const double finalEnergy,
+                                   const double initialEnergy,
+                                   const double temperature) {
   const double energyChange = finalEnergy - initialEnergy;
   return energyChange < 0 ||
          RandomNumberGenerator::getInstance().getRandomDouble(0.0, 1.0) <
              exp(-energyChange / temperature);
 }
+
+#endif // UTILS_H
